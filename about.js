60038b1ad87cc72a307c9cb50e86074f0e06ebd2
jekoob <yakov.sachuk@gmail.com>
2021-10-15 19:05:11 +0300

    Upgrde-About

--- about.js
+++ about.js
@@ -2,17 +2,20 @@
 // desgin the vc's menu 
 
 document.getElementsByTagName("li")[3].style.borderLeft="30px solid transparent";
+document.getElementsByTagName("li")[3].style.fontStyle="italic";
 function currentContent(anchoer){
     for(i=3;i<6;i++){
-    document.getElementsByTagName("li")[i].style.backgroundColor="rgba(100, 200, 300, 0.5)";
-    document.getElementsByTagName("li")[i].style.color="black";
+    document.getElementsByTagName("li")[i].style.backgroundColor="rgba(17,17,17, 0.5)";
+    //document.getElementsByTagName("li")[i].style.color="#fffdef";
     document.getElementsByTagName("li")[i].style.borderLeft="0px solid transparent";
-    document.getElementsByTagName("li")[i].style.textShadow="none";
+    document.getElementsByTagName("li")[i].style.fontStyle="normal";
+   // document.getElementsByTagName("li")[i].style.textShadow="none";
     }
 
-    document.getElementById(anchoer).style.borderBottom="30px solid rgba(100, 200, 300,1)";
+    document.getElementById(anchoer).style.borderBottom="30px solid rgba(100, 200, 300, 0.5)";
     document.getElementById(anchoer).style.borderLeft="30px solid transparent";
-    document.getElementById(anchoer).style.textShadow="white 2px 2px 2px ";
+    document.getElementById(anchoer).style.fontStyle="italic";
+    //document.getElementById(anchoer).style.textShadow="white 2px 2px 2px ";
 }
 
 let tableHeight=document.getElementsByClassName("tableContent")[0].offsetHeight;
@@ -20,14 +23,14 @@ let tableHeight=document.getElementsByClassName("tableContent")[0].offsetHeight;
 let vcHeight=document.getElementsByClassName("vc")[0].offsetHeight;
 
 let theHeight = window.outerHeight;
-
+document.getElementsByTagName("body")[0].style.height=theHeight+"px";
 let vcContent = theHeight-tableHeight;
 
 
 // pull up the vc element to top .
 function toggleUp(){
     
-    document.getElementsByClassName("proImg")[0].style.display="none";
+    document.getElementsByClassName("proImg")[0].style.opacity=0;
     document.getElementsByTagName("UL")[1].style.opacity=0;
     
     // header disappear countinuously 
@@ -40,7 +43,7 @@ function toggleUp(){
                 document.getElementsByTagName("i")[0].style.transform="rotatex(180deg)";
                 document.getElementsByTagName("i")[0].setAttribute("onclick","toggleDown()");
             }
-        }, 200);
+       }, 200);
     }
     
 }
@@ -50,7 +53,7 @@ function toggleDown(){
     document.getElementsByTagName("i")[0].style.transform="rotatex(360deg)";
     setTimeout(function(){
         document.getElementsByClassName("tableContent")[0].style.paddingTop="60px";
-        document.getElementsByClassName("proImg")[0].style.display="block";
+        document.getElementsByClassName("proImg")[0].style.opacity=1;
         document.getElementsByTagName("UL")[1].style.opacity=1;
         document.getElementsByTagName("i")[0].setAttribute("onclick","toggleUp()");
         document.getElementsByClassName("vc")[0].style.height=vcContent+"px";
@@ -60,6 +63,5 @@ function toggleDown(){
 
 
 // VC's box  fitting its height to the device screen.
-theHeight=theHeight-document.getElementsByClassName("tableContent")[0].offsetHeight;
-theHeight=theHeight+"px";
-document.getElementsByClassName("vc")[0].style.height=theHeight;
+document.getElementsByClassName("vc")[0].style.height=vcContent+"px";
+document.getElementsByClassName("tableContent")[0].style.height=tableHeight+"px";
