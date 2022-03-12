1efbe65ad7597dc79155a11ee0ae2997c68630a7
jekoob <yakov.sachuk@gmail.com>
2022-03-12 16:39:34 +0200

    repairing all slides

--- slides.js
+++ slides.js
@@ -19,32 +19,38 @@ function slideHome(event){
       }
       document.getElementById("shell").style.display="block";
       listNav[1].setAttribute("class","currentPage");
-      let blankUp = document.createElement("iframe");
+      let blankUp = document.createElement("div");
       blankUp.setAttribute("class","blank");
       blankUp.setAttribute("id","blankUp");
-      blankUp.setAttribute("src","index.html");
+      let bg_img = document.createElement("div");
+      bg_img.setAttribute("id","bg-shadow");
+      blankUp.appendChild(bg_img);
+      blankUp.style.backgroundImage="url('img/testing.png')";
       document.getElementsByTagName("body")[0].appendChild(blankUp);
       setTimeout(function(){
          blankUp.style.height="100%";
       },500);
-      setTimeout(function(){
+      // setTimeout(function(){
          
-         if(currentPage == "Home" && previous =="FrontEnd" || previous =="Automation"){
-            if(previous =="FrontEnd" && firstTime==false){
-               listNav[0].classList.remove("currentPage");
-               console.log(firstTime);
-               document.getElementById("blankRight").remove();
-            }
-            else if(previous =="Automation" && firstTime==false){
-               listNav[2].classList.remove("currentPage");
-               console.log(firstTime);
-               document.getElementById("blankLeft").remove();
-            }
-         }
-         firstTime=false;
-         currentPage=event.textContent;
-        document.getElementById("shell").style.display="none";
-      },1000);
+      //    if(currentPage == "Home" && previous =="FrontEnd" || previous =="Automation"){
+      //       if(previous =="FrontEnd" && firstTime==false){
+      //          listNav[0].classList.remove("currentPage");
+      //          console.log(firstTime);
+      //          document.getElementById("blankRight").remove();
+      //       }
+      //       else if(previous =="Automation" && firstTime==false){
+      //          listNav[2].classList.remove("currentPage");
+      //          console.log(firstTime);
+      //          document.getElementById("blankLeft").remove();
+      //       }
+      //    }
+      //    firstTime=false;
+      //    currentPage=event.textContent;
+      //   document.getElementById("shell").style.display="none";
+      // },1000);
+      setTimeout(function(){
+         window.open("index.html","_self");
+      },1200);
    }
      
       return;
@@ -53,42 +59,46 @@ function slideHome(event){
 function slideBlankRight(event){
     
    if(currentPage != event.textContent){
+      console.log("event : "+event.textContent);
     for(let i=0;i<pagesName.length;i++){
          if(pagesName[i]==currentPage){
                previous = pagesName[i];
-               console.log(previous);
+               console.log("previous : "+previous);
                listNav[i].classList.remove("currentPage");
                currentPage=event.textContent;
-               console.log(currentPage);
+               console.log("cuurentPage : "+currentPage);
                break;
          }
       }
       document.getElementById("shell").style.display="block";
       listNav[0].setAttribute("class","currentPage");
-      let blankRight = document.createElement("iframe");
+      let blankRight = document.createElement("div");
       blankRight.setAttribute("class","blank");
       blankRight.setAttribute("id","blankRight");
-      blankRight.setAttribute("src","frontEndPage.html");
+      blankRight.style.backgroundImage="url('img/front.jpg')";
       document.getElementsByTagName("body")[0].appendChild(blankRight);
       setTimeout(function(){
          blankRight.style.width="100%";
-      },500)
-      setTimeout(function(){
-         if(currentPage == "FrontEnd" && previous =="Automation" || previous=="Home"){
-             if(previous =="Automation" && firstTime==false){
-               listNav[2].classList.remove("currentPage");
-               document.getElementById("blankLeft").remove();
-            }
-            else if(previous =="Home" && firstTime==false){
-               listNav[1].classList.remove("currentPage");
-               document.getElementById("blankUp").remove();
-            }
+      },500);
+      // setTimeout(function(){
+      //    if(currentPage == "FrontEnd" && previous =="Automation" || previous=="Home"){
+      //        if(previous =="Automation" && firstTime==false){
+      //          listNav[2].classList.remove("currentPage");
+      //          document.getElementById("blankLeft").remove();
+      //       }
+      //       else if(previous =="Home" && firstTime==false){
+      //          listNav[1].classList.remove("currentPage");
+      //          document.getElementById("blankUp").remove();
+      //       }
 
-         }
-         firstTime=false;
-         currentPage=event.textContent;
-         document.getElementById("shell").style.display="none";
-      },1000);
+      //    }
+      //    firstTime=false;
+      //    currentPage=event.textContent;
+      //    document.getElementById("shell").style.display="none";
+      // },1000);
+      setTimeout(function(){
+         window.open("frontEndPage.html","_self");
+      },1350);
       return;
    }
 }
@@ -110,30 +120,33 @@ function slideBlankLeft(event){
    listNav[2].setAttribute("class","currentPage");
    document.getElementById("shell").style.display="block";
    console.log(currentPage);
-   let blankLeft = document.createElement("iframe");
+   let blankLeft = document.createElement("div");
    blankLeft.setAttribute("class","blank");
    blankLeft.setAttribute("id","blankLeft");
-   blankLeft.setAttribute("src","autoProPage.html");
+   blankLeft.style.backgroundImage="url('img/autoPro.jpg')";
    document.getElementsByTagName("body")[0].appendChild(blankLeft);
    setTimeout(function(){
          blankLeft.style.width="100%";
       },500)
-   setTimeout(function(){
+   // setTimeout(function(){
       //myLinks[1].style.opacity="1";
-    document.getElementById("shell").style.display="none";
-    if(currentPage == "Automation" && previous =="FrontEnd" || previous=="Home" ){
-         if(previous =="FrontEnd" && firstTime==false){
-            listNav[0].classList.remove("currentPage");
-            document.getElementById("blankRight").remove();
-         }
-         else if(previous =="Home" && firstTime==false){
-            listNav[1].classList.remove("currentPage");
-            document.getElementById("blankUp").remove();
-         }
-      }
-      firstTime=false;
-      currentPage=event.textContent;
-   },1000);
+   //  document.getElementById("shell").style.display="none";
+   //  if(currentPage == "Automation" && previous =="FrontEnd" || previous=="Home" ){
+   //       if(previous =="FrontEnd" && firstTime==false){
+   //          listNav[0].classList.remove("currentPage");
+   //          document.getElementById("blankRight").remove();
+   //       }
+   //       else if(previous =="Home" && firstTime==false){
+   //          listNav[1].classList.remove("currentPage");
+   //          document.getElementById("blankUp").remove();
+   //       }
+   //    }
+   //    firstTime=false;
+   //    currentPage=event.textContent;
+   // },1000);
+   setTimeout(function(){
+      window.open("autoProPage.html","_self");
+   },1350);
    return;
    }
 
