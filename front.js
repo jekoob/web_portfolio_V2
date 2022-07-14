bcc3d7c1ea5f0a91ccd4f0c1bb399436fb45b73c
jekoob <yakov.sachuk@gmail.com>
2022-07-14 16:23:40 +0300

    progress trails

--- front.js
+++ front.js
@@ -3,13 +3,17 @@ let roller = document.getElementById("roller").children;
 let gallery = document.getElementById("gallery");
 let photoFilm = document.getElementById("photoFilm");
 let currentPage = "FrontEnd"; 
-
+let proggressList=[
+	{ name:"hungman", js:"70.7%" , css:"23.8%" , html:"5.6%" },
+	{ name:"battle - ship", js:"92.7%" , css:"4.5%" , html:"2.8%" }
+]
 let rollerIndex=0;
 roller[0].style.backgroundColor="black";
 
 setTimeout(()=>{
 	this.topTitle.style.opacity="1";
 	this.gallery.style.opacity="1";
+	$("#proTitle").text(proggressList[0].name);
 },500);
 
 
@@ -33,3 +37,10 @@ function leftArrow(){
 	
 }
 
+class Item{
+	constructor(js,css,html){
+		this.js=js;
+		this.css=css;
+		this.html=html;
+	}
+}
