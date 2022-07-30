9a5dc62687e55793e3ee16a8624bfdf84a49ad9e
jekoob <yakov.sachuk@gmail.com>
2022-07-30 11:41:09 +0300

    uptade trail progrestion

--- front.js
+++ front.js
@@ -2,10 +2,21 @@ let topTitle = document.getElementById("topTitle");
 let roller = document.getElementById("roller").children;
 let gallery = document.getElementById("gallery");
 let photoFilm = document.getElementById("photoFilm");
+const jsPortion = document.querySelector("#js .portion");
+const jsTrail = document.querySelector("#js .trail")
+const cssPortion = document.querySelector("#css .portion");
+const cssTrail = document.querySelector("#css .trail");
+const htmlPortion = document.querySelector("#html .portion");
+const htmlTrail = document.querySelector("#html .trail");
+const proggressTrail = document.querySelector("#proggressTrail");
+const noteBtn = document.getElementsByClassName("noteBtn");
 let currentPage = "FrontEnd"; 
+let proNum = 0;
 let proggressList=[
-	{ name:"hungman", js:"70.7%" , css:"23.8%" , html:"5.6%" },
-	{ name:"battle - ship", js:"92.7%" , css:"4.5%" , html:"2.8%" }
+	{ name:"Hungman", js:70, css:23.8, html:5.6, paint:"white"},
+	{ name:"Battleship", js:76 , css:4 , html:20 ,paint:"white" },
+	{ name:"My vocabulary", js:82 , css:8 , html:10 ,paint:"black" },
+	{ name:"JS Quiz", js:50 , css:30 , html:20 ,paint:"white" }
 ]
 let rollerIndex=0;
 roller[0].style.backgroundColor="black";
@@ -13,7 +24,7 @@ roller[0].style.backgroundColor="black";
 setTimeout(()=>{
 	this.topTitle.style.opacity="1";
 	this.gallery.style.opacity="1";
-	$("#proTitle").text(proggressList[0].name);
+	setProgress(proNum);
 },500);
 
 
@@ -24,6 +35,8 @@ function rightArrow(){
 		roller[rollerIndex].style.backgroundColor="";
 		rollerIndex+=1;
 		roller[rollerIndex].style.backgroundColor="black";
+		proNum++;
+		setProgress(proNum);
 	}
 	
 }
@@ -33,14 +46,61 @@ function leftArrow(){
 		roller[rollerIndex].style.backgroundColor="";
 		rollerIndex-=1;
 		roller[rollerIndex].style.backgroundColor="black";
+		proNum--;
+		setProgress(proNum);
 	}
 	
 }
+function setPrecentagePortion(indx){
+	countJs=0;
+	countCss=0;
+	countHtml=0;
+	let intervalJs = setInterval(()=>{
+		if(countJs<proggressList[indx].js){
+			jsPortion.innerHTML= countJs+"%";
+			jsTrail.style.width=countJs+"%";
+			console.log(countJs+"%");
+		}
+		else{
+			clearInterval(intervalJs);
+		}
+		countJs++;
+	},2);
 
-class Item{
-	constructor(js,css,html){
-		this.js=js;
-		this.css=css;
-		this.html=html;
-	}
+	let intervalCss = setInterval(()=>{
+		if(countCss<proggressList[indx].css){
+			cssPortion.innerHTML= countCss+"%";
+			cssTrail.style.width=countCss+"%";
+			console.log(countCss+"%");
+		}
+		else{
+			clearInterval(intervalCss);
+		}
+		countCss++;
+	},2);
+	let intervalHtml = setInterval(()=>{
+		if(countHtml<proggressList[indx].html){
+			htmlPortion.innerHTML= countHtml+"%";
+			htmlTrail.style.width=countHtml+"%";
+			console.log(countHtml+"%");
+		}
+		else{
+			clearInterval(intervalHtml);
+		}
+		countHtml++;
+	},2);
 }
+
+function setProgress(index){
+	document.querySelector(".theTitle").classList.add("faid");
+	document.querySelector(".theTitle").innerHTML=proggressList[index].name;
+	setPrecentagePortion(index);
+	proggressTrail.style.color=proggressList[index].paint;
+	for (var i = 0; i < noteBtn.length; i++) {
+		noteBtn[i].style.border="2px solid "+proggressList[index].paint;
+	}
+	setTimeout(()=>{
+		$(".theTitle").removeClass("faid");
+	},300);
+}
+
