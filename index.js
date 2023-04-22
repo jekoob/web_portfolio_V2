97544937682892188c72a48aecf68736e80a17f7
jekoob <yakov.sachuk@gmail.com>
2023-04-23 00:33:52 +0300

    respinsive upgrate

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let bug = document.getElementsByClassName("bugQoute")[0];   //img of the bug.
 let wrapeQuoteWidth = document.getElementsByClassName("quote-wrap")[0].offsetWidth; 
 let quote = document.getElementById("quote");//div that contain the qoute.
 let quoteAngle = document.getElementsByClassName("angle")[0];
-
+let quoteSubAngle = document.getElementsByClassName("subAngel")[0];
 let myLinks = document.getElementsByClassName("myLinks");
 let cv=document.getElementById("cvLink");
 
@@ -27,6 +27,7 @@ setTimeout(() => {
    quote.innerHTML=quotes[0];
    quote.style.opacity=1;
    quoteAngle.style.opacity=1;
+   quoteSubAngle.style.opacity=1;
 }, 200);
 
 
@@ -40,13 +41,22 @@ setInterval(()=>{
       quoteAngle.style.transitionDelay="0s";
       quoteAngle.style.transitionDuration="0s";
       quoteAngle.style.opacity=0;
+
+      quoteSubAngle.style.transitionDelay="0s";
+      quoteSubAngle.style.transitionDuration="0s";
+      quoteSubAngle.style.opacity=0;
+
       bug.style.transform = "translateX("+(wrapeQuoteWidth-100)+"px)";
       quote.innerHTML=quotes[1];
+
+
       setTimeout(() => {
          quote.style.opacity=1;
          quote.style.transitionDuration="300ms";
          quoteAngle.style.opacity=1;
          quoteAngle.style.transform="translateX("+(-150)+"px) scaleX(-1)";
+         quoteSubAngle.style.opacity=1;
+         quoteSubAngle.style.transform="translateX("+(-150)+"px) scaleX(-1)";
       }, 1000);
     
       count++;
@@ -54,16 +64,20 @@ setInterval(()=>{
    else{
       bug.style.transform = "translateX("+(wrapeQuoteWidth+120)+"px)";
       quoteAngle.style.transform="translateX("+0+"px) scaleX(1)";
+      quoteSubAngle.style.transform="translateX("+0+"px) scaleX(1)";
       quote.style.transitionDuration="0ms";
       quote.style.opacity=0;
       quoteAngle.style.opacity=0;
+      quoteSubAngle.style.opacity=0;
       quote.innerHTML=quotes[0];
 
       setTimeout(() => {
          quote.style.opacity=1;
          quote.style.transitionDuration="300ms";
          quoteAngle.style.opacity=1;
+         quoteSubAngle.style.opacity=1;
          quoteAngle.style.transform="translateX("+(0)+"px) scaleX(1)";
+         quoteSubAngle.style.transform="translateX("+(0)+"px) scaleX(1)";
       }, 1100);
       count++;
    }
