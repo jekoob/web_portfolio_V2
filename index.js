4b73175e72250f8458c250ca2bd33f592bc6fd9a
jekoob <yakov.sachuk@gmail.com>
2023-04-25 00:59:10 +0300

    main page mdiffiyng to phone view

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let body = document.getElementsByTagName("body")[0];
 let bgImg = document.getElementById("bg-img");
 let screenWidth = document.getElementsByTagName("body")[0].offsetWidth;
 let screenHeight = document.getElementById("bg-img").offsetHeight;
-let bug = document.getElementsByClassName("bugQoute")[0];   //img of the bug.
-
+let bug1 = document.getElementsByClassName("bugQoute")[0];   //img of the bug.
+let bug2 = document.getElementsByClassName("bugQoute")[1]; 
 // outter qoute container that contains the "qoute"div and angle div.
 let wrapeQuoteWidth = document.getElementsByClassName("quote-wrap")[0].offsetWidth; 
 let quote = document.getElementById("quote");//div that contain the qoute.
@@ -18,12 +18,13 @@ let currentPage="Home";
 
 
 // fitting the length of the trail for the bug
-bug.style.transform = "translateX(-"+(screenWidth/2)+"px)"; 
-bug.style.transitionDuration="1.5s";
+// bug.style.transform = "translateX(-"+(screenWidth/2)+"px)"; 
+// bug.style.transitionDuration="1.5s";
 setTimeout(() => {
   
-   bug.style.opacity=1;
-   bug.style.transform = "translateX("+(wrapeQuoteWidth+120)+"px)";
+   // bug1.style.opacity=1;
+   bug1.style.opacity=1;
+   // bug.style.transform = "translateX("+(wrapeQuoteWidth+120)+"px)";
    quote.innerHTML=quotes[0];
    quote.style.opacity=1;
    quoteAngle.style.opacity=1;
@@ -35,9 +36,11 @@ let count =1;
 setInterval(()=>{
    
    if(count%2!=0){
+
       quote.style.transitionDelay="0s";
       quote.style.transitionDuration="0ms";
       quote.style.opacity=0;
+
       quoteAngle.style.transitionDelay="0s";
       quoteAngle.style.transitionDuration="0s";
       quoteAngle.style.opacity=0;
@@ -46,15 +49,26 @@ setInterval(()=>{
       quoteSubAngle.style.transitionDuration="0s";
       quoteSubAngle.style.opacity=0;
 
-      bug.style.transform = "translateX("+(wrapeQuoteWidth-100)+"px)";
+      bug1.style.transitionDelay="0s";
+      bug1.style.transitionDuration="0s";
+      bug1.style.opacity=0;
+
+      // bugs1.style.opacity=1;
+      // bug.style.transform = "translateX("+(wrapeQuoteWidth-100)+"px)";
       quote.innerHTML=quotes[1];
 
-
+      
+      bug2.style.transitionDelay="30ms";
+      bug2.style.transitionDuration="1s";
+      bug2.style.opacity=1;
       setTimeout(() => {
+
          quote.style.opacity=1;
          quote.style.transitionDuration="300ms";
+
          quoteAngle.style.opacity=1;
          quoteAngle.style.transform="translateX("+(-150)+"px) scaleX(-1)";
+
          quoteSubAngle.style.opacity=1;
          quoteSubAngle.style.transform="translateX("+(-150)+"px) scaleX(-1)";
       }, 1450);
@@ -62,21 +76,34 @@ setInterval(()=>{
       count++;
    }
    else{
-      bug.style.transform = "translateX("+(wrapeQuoteWidth+120)+"px)";
-      quoteAngle.style.transform="translateX("+0+"px) scaleX(1)";
-      quoteSubAngle.style.transform="translateX("+0+"px) scaleX(1)";
+      // bug.style.transform = "translateX("+(wrapeQuoteWidth+120)+"px)";
       quote.style.transitionDuration="0ms";
       quote.style.opacity=0;
-      quoteAngle.style.opacity=0;
-      quoteSubAngle.style.opacity=0;
       quote.innerHTML=quotes[0];
+
+      quoteAngle.style.transform="translateX("+0+"px) scaleX(1)";
+      quoteAngle.style.opacity=0;
+
+      quoteSubAngle.style.transform="translateX("+0+"px) scaleX(1)";
+      quoteSubAngle.style.opacity=0;
+
+      bug2.style.transitionDelay="0s";
+      bug2.style.transitionDuration="0s";
+      bug2.style.opacity=0;
+
+      bug1.style.transitionDelay="30ms";
+      bug1.style.transitionDuration="1s";
+      bug1.style.opacity=1;
+     
 
       setTimeout(() => {
          quote.style.opacity=1;
          quote.style.transitionDuration="300ms";
+
          quoteAngle.style.opacity=1;
-         quoteSubAngle.style.opacity=1;
          quoteAngle.style.transform="translateX("+(0)+"px) scaleX(1)";
+
+         quoteSubAngle.style.opacity=1;
          quoteSubAngle.style.transform="translateX("+(0)+"px) scaleX(1)";
       }, 1450);
       count++;
