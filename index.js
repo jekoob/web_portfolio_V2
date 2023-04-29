a7e0f7e650ba5734673ac4f6bcade93b4865fac4
jekoob <yakov.sachuk@gmail.com>
2023-04-29 19:02:42 +0300

    mdiffting main page with scroll-arrow feature
    Signed-off-by: jekoob <yakov.sachuk@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const quotes = ["“Quality is never an accident; it is always the result of intelligent effort.”",
-                "“Software never was perfect and won't get perfect."];
+const quotes = ["“Quality is never an accident; it is always the result of intelligent effort.”","“Software never was perfect and won't get perfect."];
 let body = document.getElementsByTagName("body")[0];
 let bgImg = document.getElementById("bg-img");
 let screenWidth = document.getElementsByTagName("body")[0].offsetWidth;
@@ -9,29 +8,52 @@ let bug2 = document.getElementsByClassName("bugQoute")[1];
 // outter qoute container that contains the "qoute"div and angle div.
 let wrapeQuoteWidth = document.getElementsByClassName("quote-wrap")[0].offsetWidth; 
 let quote = document.getElementById("quote");//div that contain the qoute.
-let quoteAngle = document.getElementsByClassName("angle")[0];
-let quoteSubAngle = document.getElementsByClassName("subAngel")[0];
+// let quoteAngle = document.getElementsByClassName("angle")[0];
+// let quoteSubAngle = document.getElementsByClassName("quoteSubAngle")[0];
+let quoteAngleRight = document.getElementById("angleRight");
+let quoteSubAngleRight = document.getElementById("subAngelRight");
+let quoteAngleLeft = document.getElementById("angleLeft");
+let quoteSubAngleLeft = document.getElementById("subAngeLeft");
 let myLinks = document.getElementsByClassName("myLinks");
 let cv=document.getElementById("cvLink");
 
 let currentPage="Home";
 
+// const running = quote.style.animationPlayState === 'running';
 
+setTimeout(()=>{
+   let count=1;
+   setInterval(()=>{
+      switch(count){
+         case 0:
+            quote.innerHTML=quotes[count];
+            count++;
+            break;           
+         case 1:
+            quote.innerHTML=quotes[count];
+            count=0;
+            break;
+      }
+      // quote.style.animationPlayState = running ? 'paused' : 'running';
+   },4000);
+},0);
+quote.innerHTML=quotes[0];
 // fitting the length of the trail for the bug
 // bug.style.transform = "translateX(-"+(screenWidth/2)+"px)"; 
 // bug.style.transitionDuration="1.5s";
-setTimeout(() => {
+// setTimeout(() => {
   
-   // bug1.style.opacity=1;
-   bug1.style.opacity=1;
-   // bug.style.transform = "translateX("+(wrapeQuoteWidth+120)+"px)";
-   quote.innerHTML=quotes[0];
-   quote.style.opacity=1;
-   quoteAngle.style.opacity=1;
-   quoteSubAngle.style.opacity=1;
-}, 200);
+//    // bug1.style.opacity=1;
+//    // bug1.style.opacity=1;
+//    // bug.style.transform = "translateX("+(wrapeQuoteWidth+120)+"px)";
 
+//    // quote.style.opacity=1;
+//    // quoteAngleRight.style.opacity=1;
+//    // quoteSubAngleRight.style.opacity=1;
+//    quote.style.animationPlayState = running ? 'paused' : 'running';
+// }, 1000);
 
+/*
 let count =1;
 setInterval(()=>{
    
@@ -41,13 +63,13 @@ setInterval(()=>{
       quote.style.transitionDuration="0ms";
       quote.style.opacity=0;
 
-      quoteAngle.style.transitionDelay="0s";
-      quoteAngle.style.transitionDuration="0s";
-      quoteAngle.style.opacity=0;
+      // quoteAngle.style.transitionDelay="0s";
+      // quoteAngle.style.transitionDuration="0s";
+      // quoteAngle.style.opacity=0;
 
-      quoteSubAngle.style.transitionDelay="0s";
-      quoteSubAngle.style.transitionDuration="0s";
-      quoteSubAngle.style.opacity=0;
+      // quoteSubAngle.style.transitionDelay="0s";
+      // quoteSubAngle.style.transitionDuration="0s";
+      // quoteSubAngle.style.opacity=0;
 
       bug1.style.transitionDelay="0s";
       bug1.style.transitionDuration="0s";
@@ -66,11 +88,11 @@ setInterval(()=>{
          quote.style.opacity=1;
          quote.style.transitionDuration="300ms";
 
-         quoteAngle.style.opacity=1;
-         quoteAngle.style.transform="translateX("+(-150)+"px) scaleX(-1)";
+         // quoteAngleLeft.style.opacity=1;
+         // quoteAngleLeft.style.transform="translateX("+(-150)+"px) scaleX(-1)";
 
-         quoteSubAngle.style.opacity=1;
-         quoteSubAngle.style.transform="translateX("+(-150)+"px) scaleX(-1)";
+         // quoteSubAngleLeft.style.opacity=1;
+         // quoteSubAngle.style.transform="translateX("+(-150)+"px) scaleX(-1)";
       }, 1450);
     
       count++;
@@ -81,11 +103,11 @@ setInterval(()=>{
       quote.style.opacity=0;
       quote.innerHTML=quotes[0];
 
-      quoteAngle.style.transform="translateX("+0+"px) scaleX(1)";
-      quoteAngle.style.opacity=0;
+      // quoteAngle.style.transform="translateX("+0+"px) scaleX(1)";
+      // quoteAngle.style.opacity=0;
 
-      quoteSubAngle.style.transform="translateX("+0+"px) scaleX(1)";
-      quoteSubAngle.style.opacity=0;
+      // quoteSubAngle.style.transform="translateX("+0+"px) scaleX(1)";
+      // quoteSubAngle.style.opacity=0;
 
       bug2.style.transitionDelay="0s";
       bug2.style.transitionDuration="0s";
@@ -100,13 +122,13 @@ setInterval(()=>{
          quote.style.opacity=1;
          quote.style.transitionDuration="300ms";
 
-         quoteAngle.style.opacity=1;
-         quoteAngle.style.transform="translateX("+(0)+"px) scaleX(1)";
+         // quoteAngle.style.opacity=1;
+         // quoteAngle.style.transform="translateX("+(0)+"px) scaleX(1)";
 
-         quoteSubAngle.style.opacity=1;
-         quoteSubAngle.style.transform="translateX("+(0)+"px) scaleX(1)";
+         // quoteSubAngle.style.opacity=1;
+         // quoteSubAngle.style.transform="translateX("+(0)+"px) scaleX(1)";
       }, 1450);
       count++;
    }
 }, 5000);
-
+*/
